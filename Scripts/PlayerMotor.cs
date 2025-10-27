13289c6111199035d1973e417fd7c11776949b61
kalen-lewis <102082347+kalen-lewis@users.noreply.github.com>
2025-10-27 18:03:59 -0500

    First commit (Kalen)

---
+++ Scripts/PlayerMotor.cs
@@ -0,0 +1,100 @@
+/*using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMotor : MonoBehaviour
+{
+
+    private CharacterController controller;
+    private Vector3 playerVelocity;
+    private bool isGrounded;
+    public float speed = 5.0f;
+    public float gravity = -9.8f;
+    public float jumpHeight = 3.0f;
+
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        isGrounded = controller.isGrounded;
+    }
+    //gets inputs from input manager script, puts onto character controller
+    public void ProcessMove(Vector2 input)
+    {
+        Vector3 moveDirection = Vector3.zero;
+        moveDirection.x = input.x;
+        moveDirection.z = input.y;
+        controller.Move(transform.TransformDirection(moveDirection) * speed * Time.deltaTime);
+        playerVelocity.y += gravity * Time.deltaTime;
+        if (isGrounded && playerVelocity.y < 0)
+            playerVelocity.y = -2.0f;
+        controller.Move(playerVelocity * Time.deltaTime);
+        Debug.Log(playerVelocity.y);
+    }
+
+    public void Jump()
+    {
+        if (isGrounded)
+        {
+            playerVelocity.y = Mathf.Sqrt(jumpHeight * -3.0f * gravity);
+        }
+    }
+}
+
+    */
+
+
+using UnityEngine.InputSystem;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+public class PlayerMotor : MonoBehaviour
+{
+    private CharacterController controller;
+    private Vector3 playerVelocity;
+    private bool isGrounded;
+
+    [Header("Movement Settings")]
+    public float speed = 5.0f;
+    public float gravity = -9.8f;
+    public float jumpHeight = 3.0f;
+
+    private void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    private void Update()
+    {
+        isGrounded = controller.isGrounded;
+    }
+
+    public void ProcessMove(Vector2 input)
+    {
+        Vector3 moveDirection = new Vector3(input.x, 0, input.y);
+        controller.Move(transform.TransformDirection(moveDirection) * speed * Time.deltaTime);
+
+        playerVelocity.y += gravity * Time.deltaTime;
+
+        if (isGrounded && playerVelocity.y < 0)
+            playerVelocity.y = -2.0f;
+
+        controller.Move(playerVelocity * Time.deltaTime);
+    }
+
+    public void Jump()
+    {
+        if (isGrounded)
+        {
+            playerVelocity.y = Mathf.Sqrt(jumpHeight * -3.0f * gravity);
+        }
+    }
+}
+
