1b1c85df5b6c3cbdc8673ecf7a9015e653b0f6c7
kalen-lewis <102082347+kalen-lewis@users.noreply.github.com>
2025-11-12 17:31:47 -0600

    kalen second commit

---
+++ Assets/Scripts/InputManager.cs
@@ -0,0 +1,50 @@
+
+//Player Motor
+//using UnityEngine.InputSystem;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+public class PlayerMotor : MonoBehaviour
+{
+    private CharacterController controller;
+    private Vector3 playerVelocity;
+    private bool isGrounded;
+
+    [Header("Movement Settings")]
+    public float speed = 5.0f;
+    public float gravity = -9.8f;
+    public float jumpHeight = 3.0f;
+
+    private void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    private void Update()
+    {
+        isGrounded = controller.isGrounded;
+    }
+
+    public void ProcessMove(Vector2 input)
+    {
+        Vector3 moveDirection = new Vector3(input.x, 0, input.y);
+        controller.Move(transform.TransformDirection(moveDirection) * speed * Time.deltaTime);
+
+        playerVelocity.y += gravity * Time.deltaTime;
+
+        if (isGrounded && playerVelocity.y < 0)
+            playerVelocity.y = -2.0f;
+
+        controller.Move(playerVelocity * Time.deltaTime);
+    }
+
+    public void Jump()
+    {
+        if (isGrounded)
+        {
+            playerVelocity.y = Mathf.Sqrt(jumpHeight * -3.0f * gravity);
+        }
+    }
+}
